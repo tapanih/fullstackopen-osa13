ce8a80b3ef791df57d443cf08c0f2f06df8ccf8d
Tapani Honkanen <tapani.honkanen@hotmail.com>
2021-12-29 21:12:09 +0200

    Tehtävä 13.5

---
+++ controllers/blogs.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const {Blog} = require('../models');
+
+router.get('/', async (req, res) => {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body);
+        res.json(blog);
+    } catch (error) {
+        res.status(400).json({error});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const blog = await Blog.findByPk(req.params.id);
+    if (blog) {
+        await blog.destroy();
+        res.json(blog);
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Blog = require('./blog');
+
+void Blog.sync();
+
+module.exports = {Blog};
