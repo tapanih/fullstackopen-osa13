94fd697bae2e309bc544e6fe54bf94a47125f27f
Tapani Honkanen <tapani.honkanen@hotmail.com>
2022-01-01 15:08:12 +0200

    Tehtävä 13.13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const {Op} = require("sequelize");
 const {tokenExtractor} = require("../util/middleware");
 const router = require("express").Router();
 
@@ -11,12 +12,21 @@ const blogFinder = async (req, res, next) => {
 };
 
 router.get("/", async (req, res) => {
+    const where = {};
+
+    if (req.query.search) {
+        where.title = {
+            [Op.iLike]: `%${req.query.search}%`,
+        };
+    }
+
     const blogs = await Blog.findAll({
         attributes: {exclude: ["userId"]},
         include: {
             model: User,
             attributes: ["name"],
         },
+        where,
     });
     res.json(blogs);
 });
