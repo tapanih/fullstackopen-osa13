b289c29dbe69f8ec36eaaddf5c3d2d13c5836b7b
Tapani Honkanen <tapani.honkanen@hotmail.com>
2022-01-01 15:27:05 +0200

    Tehtävä 13.15

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -32,6 +32,9 @@ router.get("/", async (req, res) => {
             attributes: ["name"],
         },
         where,
+        order: [
+            ["likes", "DESC"],
+        ],
     });
     res.json(blogs);
 });
