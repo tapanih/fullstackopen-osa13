d54caa8f56ac147596ef62e7fb62169556f2a92b
Tapani Honkanen <tapani.honkanen@hotmail.com>
2022-01-06 11:27:26 +0200

    Tehtävä 13.22

--- controllers/readingLists.js
+++ controllers/readingLists.js
@@ -1,8 +1,18 @@
 const ReadingList = require("../models/readingList");
+const {tokenExtractor} = require("../util/middleware");
 const router = require("express").Router();
 
 router.post("/", async (req, res) => {
     const entry = await ReadingList.create({blogId: req.body.blogId, userId: req.body.userId});
+    res.json(entry);
+});
+
+router.put("/:id", tokenExtractor, async (req, res) => {
+    const entry = await ReadingList.findByPk(req.params.id);
+    if (!entry) throw Error("not found");
+    if (entry.userId !== req.decodedToken.id) throw Error("unauthorized");
+    entry.read = req.body.read;
+    await entry.save();
     res.json(entry);
 });
 
